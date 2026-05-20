0224e854db86880ce6781e7b4c1e76459947183d
Pierson Lim <piersonlimas12@gmail.com>
2026-05-20 21:36:10 +0800

    chore: scaffold jakpost_scraper package

---
+++ jakpost_scraper/__init__.py
@@ -0,0 +1,3 @@
+"""Scrape and summarize The Jakarta Post articles."""
+
+__version__ = "0.1.0"
---
+++ tests/test_smoke.py
@@ -0,0 +1,4 @@
+def test_package_imports():
+    import jakpost_scraper
+
+    assert jakpost_scraper.__version__ == "0.1.0"
